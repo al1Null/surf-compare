32bf123080764b25f008e9300ba779605d9c92cf
al <camenergy8@gmail.com>
2018-12-31 10:15:46 -0700

    Update CompareMaps.py

--- CompareMaps.py
+++ CompareMaps.py
@@ -2,14 +2,20 @@ import json
 
 class CompareMaps():
 
-	COMPARE_DATA_PATH = 'compare.json'
 	MAPS_PATH = 'maps.txt'
+	with open(MAPS_PATH, 'r') as fo:
+		maps = list(map_.strip() for map_ in fo.readlines())
 
-	def __init__(self):
-		with open(self.COMPARE_DATA_PATH, 'r') as fo:
-			next(fo)
+	TOTAL_MAPS_COUNT = len(maps)
+
+	def __init__(self, FORMATTED_DATA_PATH):
+		""""""
+		self.FORMATTED_DATA_PATH = FORMATTED_DATA_PATH
+
+		with open(self.FORMATTED_DATA_PATH, 'r') as fo:
 			self.compare_data = json.load(fo)
 
+		### getting the names of the players
 		players = list( self.compare_data[0]['ranks'].keys() )
 		self.player1 = players[0]
 		self.player2 = players[1]
@@ -17,30 +23,41 @@ class CompareMaps():
 		self.player1_completed_maps = []
 		self.player2_completed_maps = []
 
+		### finds completed maps for each player
 		for entry in self.compare_data:
 			if entry['ranks'][self.player1] != 'N/A':
 				self.player1_completed_maps.append(entry['map'])
 			if entry['ranks'][self.player2] != 'N/A':
 				self.player2_completed_maps.append(entry['map'])
 
-		with open(self.MAPS_PATH, 'r') as fo:
-			self.maps = list(map_.strip() for map_ in fo.readlines())
 
-	def totalCompletedMaps(self):
-		"""
-		@return a tuple of # of completed maps index 0 - p1, index 1 - p2
-		"""
-		player1_completed_maps_count = len(self.player1_completed_maps)
-		player2_completed_maps_count = len(self.player2_completed_maps)
 
-		return (player1_completed_maps_count, player2_completed_maps_count)
+	def completedMaps(self, player):
+		"""
+		@param player (int) player 1 or 2
+		"""
+		# case statement
+		if player == 1:
+			return self.player1_completed_maps
+		elif player == 2:
+			return self.player2_completed_maps
+
+	def uncompletedMaps(self, player):
+		""""""
+		# case statement
+		if player == 1:
+			uncompletedMaps = [map_ for map_ in CompareMaps.maps if map_ not in self.player1_completed_maps]
+			return uncompletedMaps
+		elif player == 2:
+			uncompletedMaps = [map_ for map_ in CompareMaps.maps if map_ not in self.player2_completed_maps]
+			return uncompletedMaps
 
 
 	def neitherCompletedMaps(self):
 		""""""
 		neither_completed = []
 
-		for map_ in self.maps:
+		for map_ in CompareMaps.maps:
 			if (map_ not in self.player1_completed_maps) and (map_ not in self.player2_completed_maps):
 				neither_completed.append(map_)
 
@@ -50,45 +67,27 @@ class CompareMaps():
 		""""""
 		both_completed = []
 
-		for map_ in self.maps:
+		for map_ in CompareMaps.maps:
 			if (map_ in self.player1_completed_maps) and (map_ in self.player2_completed_maps):
 				both_completed.append(map_)
 
 		return both_completed
 
 
-	def uniquePlayer1CompletedMaps(self):
+	def uniqueCompletedMaps(self, player):
 		""""""
-		unique_player1_completed = []
+		unique_completed_maps = []
 
-		for map_ in self.maps:
-			if (map_ in self.player1_completed_maps) and (map_ not in self.player2_completed_maps):
-				unique_player1_completed.append(map_)
+		if player == 1:
+			for map_ in CompareMaps.maps:
+				if (map_ in self.player1_completed_maps) and (map_ not in self.player2_completed_maps):
+					unique_completed_maps.append(map_)
+			return unique_completed_maps
 
-		return unique_player1_completed
+		elif player == 2:
+			for map_ in CompareMaps.maps:
+				if (map_ in self.player2_completed_maps) and (map_ not in self.player1_completed_maps):
+					unique_completed_maps.append(map_)
+			return unique_completed_maps
 
-
-	def uniquePlayer2CompletedMaps(self):
-		""""""
-		unique_player2_completed = []
-
-		for map_ in self.maps:
-			if (map_ not in self.player1_completed_maps) and (map_ in self.player2_completed_maps):
-				unique_player2_completed.append(map_)
-
-		return unique_player2_completed
-
-
-
-	def __str__(self):
-		return str(self.compare_data)
-
-
-if __name__ == '__main__':
-	c = CompareMaps()
-	print(c)
-	print(c.player1)
-	print(c.player2)
-	print(c.totalCompletedMaps())
-	print(c.neitherCompletedMaps())
-	print(c.bothCompletedMaps())
+			
