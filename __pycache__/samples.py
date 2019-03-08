54b2be074b0928110e80b3359afa258f834e82a0
al <camenergy8@gmail.com>
2019-03-07 21:59:37 -0700

    Create samples.py

---
+++ __pycache__/samples.py
@@ -0,0 +1,59 @@
+surf_4head_csgo, Time: 01:07:13, Rank: 42/74
+surf_ace_fix, Time: 00:44:14, Rank: 39/3759
+surf_aether_csgo, Time: 00:50:49, Rank: 32/117
+...
+...
+... 
+surf_water-run_banjo_skill, Time: 00:38:87, Rank: 76/7157
+surf_waterworks, Time: 01:21:52, Rank: 32/658
+surf_year3000, Time: 00:17:56, Rank: 35/1497
+
+{
+    'map': 'surf_map_name',
+    'times': {
+        'player1': ['normal_time', seconds_time],
+        'player2': ['normal_time', seconds_time]
+    },
+    'ranks': {
+        'player1': ['normal_rank', rank_percentage],
+        'player2': ['normal_rank', rank_percentage]
+    }
+}
+
+
+[
+    {
+        "map": "surf_4head_csgo",
+        "ranks": {
+            "albert": ["42/74", 56.76],
+            "peter7": ["74/74", 100.0]
+        },
+        "times": {
+            "albert": ["01:07:13", 67.13],
+            "peter7": ["01:36:53", 96.53]
+        }
+    },
+    {
+        "map": "surf_ace_fix",
+        "ranks": {...},
+        "times": {...}
+    },
+     ...
+]
+
+    ... 
+    {
+        "map": "surf_bonkers_v2",
+        "ranks": {
+            "terry6": "N/A",
+            "albert": ["7/746",0.94]
+        },
+        "times": {
+            "terry6": "N/A",
+            "albert": ["00:41:41",41.41]
+        }
+    },
+    ...
+
+
+
